64a68899e094ad5cc186c33fd67854a39ff6fb4c
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2022-11-16 03:38:02 +0300

    assignment complete

---
+++ index.js
@@ -0,0 +1,23 @@
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+ }
+ console.log(introduction("Aki"))
+ 
+ function introductionWithLanguage(name, language){
+     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+ }
+ console.log(introductionWithLanguage("Aki", "Ember.js"))
+ 
+ 
+ function introductionWithLanguageOptional(name, language = "Javascript"){
+   
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+console.log(introductionWithLanguageOptional("Gracie"))
+
+ 
+ function introductionWithLanguageOptional(name, language){
+     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+ }
+ console.log(introductionWithLanguageOptional("Gracie", "Python"))
